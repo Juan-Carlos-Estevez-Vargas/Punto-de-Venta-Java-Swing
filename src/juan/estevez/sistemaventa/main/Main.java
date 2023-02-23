314537644e2ca2e72f25f0e109d50cd3a8828af7
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-02-23 14:30:32 -0500

    Actualización íconos de la aplicación

---
+++ src/juan/estevez/sistemaventa/main/Main.java
@@ -0,0 +1,14 @@
+package juan.estevez.sistemaventa.main;
+
+import juan.estevez.sistemaventa.vista.Login;
+
+/**
+ *
+ * @author Juan Carlos Estevez Vargas
+ */
+public class Main {
+    public static void main(String[] args) {
+        Login login = new Login();
+        login.setVisible(true);
+    }
+}
