bc844fdb78c3547dc5bdc0ff6b8e8721401da57f
Juan Carlos Estevez Vargas <juank2001estevez@gmail.com>
2023-06-08 22:13:06 -0500

    optimización reporte en PDF

--- src/juan/estevez/sistemaventa/reportes/ReporteVentaPDF.java
+++ src/juan/estevez/sistemaventa/reportes/ReporteVentaPDF.java
@@ -16,6 +16,7 @@ import java.awt.Desktop;
 import java.io.File;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.sql.SQLException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import javax.swing.JFileChooser;
@@ -23,169 +24,165 @@ import javax.swing.JTable;
 import juan.estevez.sistemaventa.daos.VentaDAO;
 
 /**
- *
+ * Clase para generar un reporte en PDF de la venta realizada. Requiere la
+ * librería iTextPDF en el classpath.
+ * 
  * @author Juan Carlos Estevez Vargas.
  */
 public class ReporteVentaPDF {
 
-    VentaDAO ventaDao = new VentaDAO();
+	private VentaDAO ventaDao = new VentaDAO();
 
-    /**
-     * Genera reporte en pdf de la venta realizada.
-     *
-     * @param txtRutEmpresa
-     * @param txtNombreEmpresa
-     * @param txtTelefonoEmpresa
-     * @param txtDireccionEmpresa
-     * @param txtRazonSocialEmpresa
-     * @param txtDniRutVenta
-     * @param txtNombreClienteVenta
-     * @param txtTelefonoClienteVenta
-     * @param txtDireccionClienteVenta
-     * @param tableVenta
-     * @param totalPagar
-     */
-    public void pdf(String txtRutEmpresa, String txtNombreEmpresa, String txtTelefonoEmpresa,
-            String txtDireccionEmpresa, String txtRazonSocialEmpresa, String txtDniRutVenta,
-            String txtNombreClienteVenta, String txtTelefonoClienteVenta, String txtDireccionClienteVenta,
-            JTable tableVenta, double totalPagar) {
+	/**
+	 * Genera y abre un reporte de venta en formato PDF.
+	 * 
+	 * @param txtRutEmpresa            el RUT de la empresa.
+	 * @param txtNombreEmpresa         el nombre de la empresa.
+	 * @param txtTelefonoEmpresa       el teléfono de la empresa.
+	 * @param txtDireccionEmpresa      la dirección de la empresa.
+	 * @param txtRazonSocialEmpresa    la razón social de la empresa.
+	 * @param txtDniRutVenta           el DNI/RUT del cliente de la venta.
+	 * @param txtNombreClienteVenta    el nombre del cliente de la venta.
+	 * @param txtTelefonoClienteVenta  el teléfono del cliente de la venta.
+	 * @param txtDireccionClienteVenta la dirección del cliente de la venta.
+	 * @param tableVenta               la tabla que contiene los detalles de los
+	 *                                 productos vendidos.
+	 * @param totalPagar               el total a pagar de la venta.
+	 * @throws DocumentException si ocurre un error al generar el documento PDF.
+	 * @throws IOException       si ocurre un error al acceder al archivo PDF.
+	 * @throws SQLException 
+	 */
+	public void generarReporteVenta(String txtRutEmpresa, String txtNombreEmpresa, String txtTelefonoEmpresa,
+			String txtDireccionEmpresa, String txtRazonSocialEmpresa, String txtDniRutVenta,
+			String txtNombreClienteVenta, String txtTelefonoClienteVenta, String txtDireccionClienteVenta,
+			JTable tableVenta, double totalPagar) throws DocumentException, IOException, SQLException {
 
-        Document documento = new Document();
-        try {
+		JFileChooser fc = new JFileChooser();
+		int response = fc.showSaveDialog(tableVenta);
 
-            JFileChooser fc = new JFileChooser();
-            int response = fc.showSaveDialog(tableVenta);
+		if (response == JFileChooser.APPROVE_OPTION) {
+			File chosenFile = fc.getSelectedFile();
+			FileOutputStream archivo = new FileOutputStream(chosenFile);
 
-            if (response == JFileChooser.APPROVE_OPTION) {
-                File chosenFile = fc.getSelectedFile();
-                PdfWriter.getInstance(documento, new FileOutputStream(chosenFile + ".pdf"));         
+			Document documento = new Document();
+			PdfWriter.getInstance(documento, archivo);
 
-                int idVenta = ventaDao.idVenta();
-                FileOutputStream archivo;
-                archivo = new FileOutputStream(chosenFile);
-    
-                PdfWriter.getInstance(documento, archivo);
-                documento.open();
-                Image img = Image.getInstance("src/juan/estevez/sistemaventa/img/logo.png");
+			documento.open();
+			Image img = Image.getInstance("src/juan/estevez/sistemaventa/img/logo.png");
 
-                Paragraph fecha = new Paragraph();
-                Font negrita = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD, BaseColor.BLUE);
-                fecha.add(Chunk.NEWLINE);
-                Date date = new Date();
-                fecha.add("Factura N" + idVenta + "\n" + "Fecha: " + new SimpleDateFormat("dd-MM-yyyy").format(date) + "\n\n");
+			Paragraph fecha = new Paragraph();
+			Font negrita = new Font(Font.FontFamily.TIMES_ROMAN, 12, Font.BOLD, BaseColor.BLUE);
+			fecha.add(Chunk.NEWLINE);
+			Date date = new Date();
+			fecha.add("Factura N°" + ventaDao.obtenerIdVenta() + "\n" + "Fecha: "
+					+ new SimpleDateFormat("dd-MM-yyyy").format(date) + "\n\n");
 
-                PdfPTable encabezado = new PdfPTable(4);
-                encabezado.setWidthPercentage(100);
-                encabezado.getDefaultCell().setBorder(0);
-                float[] columnaEncabezado = new float[]{20f, 40f, 70f, 40f};
-                encabezado.setWidths(columnaEncabezado);
-                encabezado.setHorizontalAlignment(Element.ALIGN_LEFT);
-                encabezado.addCell(img);
-                String Rut = txtRutEmpresa;
-                String nombre = txtNombreEmpresa;
-                String telefono = txtTelefonoEmpresa;
-                String direccion = txtDireccionEmpresa;
-                String razpnSocial = txtRazonSocialEmpresa;
-                encabezado.addCell("");
-                encabezado.addCell("Rut: " + Rut + "\nNombre: " + nombre + "\nTelÃ©fono: " + telefono + "\nDirecciÃ³n: " + direccion + "\nRazÃ³n Social: " + razpnSocial);
-                encabezado.addCell(fecha);
-                documento.add(encabezado);
+			PdfPTable encabezado = new PdfPTable(4);
+			encabezado.setWidthPercentage(100);
+			encabezado.getDefaultCell().setBorder(0);
+			float[] columnaEncabezado = new float[] { 20f, 40f, 70f, 40f };
+			encabezado.setWidths(columnaEncabezado);
+			encabezado.setHorizontalAlignment(Element.ALIGN_LEFT);
+			encabezado.addCell(img);
+			encabezado.addCell("");
+			encabezado.addCell(
+					"Rut: " + txtRutEmpresa + "\nNombre: " + txtNombreEmpresa + "\nTeléfono: " + txtTelefonoEmpresa
+							+ "\nDirección: " + txtDireccionEmpresa + "\nRazón Social: " + txtRazonSocialEmpresa);
+			encabezado.addCell(fecha);
+			documento.add(encabezado);
 
-                Paragraph paragraphCliente = new Paragraph();
-                paragraphCliente.add(Chunk.NEWLINE);
-                paragraphCliente.add("Datos de los clientes \n\n");
-                documento.add(paragraphCliente);
+			Paragraph paragraphCliente = new Paragraph();
+			paragraphCliente.add(Chunk.NEWLINE);
+			paragraphCliente.add("Datos del cliente \n\n");
+			documento.add(paragraphCliente);
 
-                PdfPTable tablaClientes = new PdfPTable(4);
-                tablaClientes.setWidthPercentage(100);
-                tablaClientes.getDefaultCell().setBorder(0);
-                float[] columnaCliente = new float[]{20f, 50f, 30f, 40f};
-                tablaClientes.setWidths(columnaCliente);
-                tablaClientes.setHorizontalAlignment(Element.ALIGN_LEFT);
+			PdfPTable tablaClientes = new PdfPTable(4);
+			tablaClientes.setWidthPercentage(100);
+			tablaClientes.getDefaultCell().setBorder(0);
+			float[] columnaCliente = new float[] { 20f, 50f, 30f, 40f };
+			tablaClientes.setWidths(columnaCliente);
+			tablaClientes.setHorizontalAlignment(Element.ALIGN_LEFT);
 
-                PdfPCell celdaRutCliente = new PdfPCell(new Phrase("DNI/RUT", negrita));
-                PdfPCell celdaNombreCliente = new PdfPCell(new Phrase("Nombre", negrita));
-                PdfPCell celdaTelefonoCliente = new PdfPCell(new Phrase("TelÃ©fono", negrita));
-                PdfPCell celdaDireccionCliente = new PdfPCell(new Phrase("DirecciÃ³n", negrita));
+			PdfPCell celdaRutCliente = new PdfPCell(new Phrase("DNI/RUT", negrita));
+			PdfPCell celdaNombreCliente = new PdfPCell(new Phrase("Nombre", negrita));
+			PdfPCell celdaTelefonoCliente = new PdfPCell(new Phrase("Teléfono", negrita));
+			PdfPCell celdaDireccionCliente = new PdfPCell(new Phrase("Dirección", negrita));
 
-                celdaRutCliente.setBorder(0);
-                celdaNombreCliente.setBorder(0);
-                celdaTelefonoCliente.setBorder(0);
-                celdaDireccionCliente.setBorder(0);
+			celdaRutCliente.setBorder(0);
+			celdaNombreCliente.setBorder(0);
+			celdaTelefonoCliente.setBorder(0);
+			celdaDireccionCliente.setBorder(0);
 
-                tablaClientes.addCell(celdaRutCliente);
-                tablaClientes.addCell(celdaNombreCliente);
-                tablaClientes.addCell(celdaTelefonoCliente);
-                tablaClientes.addCell(celdaDireccionCliente);
+			tablaClientes.addCell(celdaRutCliente);
+			tablaClientes.addCell(celdaNombreCliente);
+			tablaClientes.addCell(celdaTelefonoCliente);
+			tablaClientes.addCell(celdaDireccionCliente);
 
-                tablaClientes.addCell(txtDniRutVenta);
-                tablaClientes.addCell(txtNombreClienteVenta);
-                tablaClientes.addCell(txtTelefonoClienteVenta);
-                tablaClientes.addCell(txtDireccionClienteVenta);
-                documento.add(tablaClientes);
+			tablaClientes.addCell(txtDniRutVenta);
+			tablaClientes.addCell(txtNombreClienteVenta);
+			tablaClientes.addCell(txtTelefonoClienteVenta);
+			tablaClientes.addCell(txtDireccionClienteVenta);
+			documento.add(tablaClientes);
 
-                // Productos
-                PdfPTable tablaProductos = new PdfPTable(4);
-                tablaProductos.setWidthPercentage(100);
-                tablaProductos.getDefaultCell().setBorder(0);
-                float[] columnaProducto = new float[]{20f, 50f, 25f, 30f};
-                tablaProductos.setWidths(columnaProducto);
-                tablaProductos.setHorizontalAlignment(Element.ALIGN_LEFT);
+			// Productos
+			PdfPTable tablaProductos = new PdfPTable(4);
+			tablaProductos.setWidthPercentage(100);
+			tablaProductos.getDefaultCell().setBorder(0);
+			float[] columnaProducto = new float[] { 20f, 50f, 25f, 30f };
+			tablaProductos.setWidths(columnaProducto);
+			tablaProductos.setHorizontalAlignment(Element.ALIGN_LEFT);
 
-                PdfPCell celdaCantidadProducto = new PdfPCell(new Phrase("Cantidad", negrita));
-                PdfPCell celdaDescripciÃ³nProducto = new PdfPCell(new Phrase("DescripciÃ³n", negrita));
-                PdfPCell celdaPrecioUnitario = new PdfPCell(new Phrase("Precio U.", negrita));
-                PdfPCell celdaPrecioTotal = new PdfPCell(new Phrase("Precio T.", negrita));
+			PdfPCell celdaCantidadProducto = new PdfPCell(new Phrase("Cantidad", negrita));
+			PdfPCell celdaDescripcionProducto = new PdfPCell(new Phrase("Descripción", negrita));
+			PdfPCell celdaPrecioUnitario = new PdfPCell(new Phrase("Precio U.", negrita));
+			PdfPCell celdaPrecioTotal = new PdfPCell(new Phrase("Precio T.", negrita));
 
-                celdaCantidadProducto.setBorder(0);
-                celdaDescripciÃ³nProducto.setBorder(0);
-                celdaPrecioUnitario.setBorder(0);
-                celdaPrecioTotal.setBorder(0);
+			celdaCantidadProducto.setBorder(0);
+			celdaDescripcionProducto.setBorder(0);
+			celdaPrecioUnitario.setBorder(0);
+			celdaPrecioTotal.setBorder(0);
 
-                celdaCantidadProducto.setBackgroundColor(BaseColor.GRAY);
-                celdaDescripciÃ³nProducto.setBackgroundColor(BaseColor.GRAY);
-                celdaPrecioUnitario.setBackgroundColor(BaseColor.GRAY);
-                celdaPrecioTotal.setBackgroundColor(BaseColor.GRAY);
+			celdaCantidadProducto.setBackgroundColor(BaseColor.GRAY);
+			celdaDescripcionProducto.setBackgroundColor(BaseColor.GRAY);
+			celdaPrecioUnitario.setBackgroundColor(BaseColor.GRAY);
+			celdaPrecioTotal.setBackgroundColor(BaseColor.GRAY);
 
-                tablaProductos.addCell(celdaCantidadProducto);
-                tablaProductos.addCell(celdaDescripciÃ³nProducto);
-                tablaProductos.addCell(celdaPrecioUnitario);
-                tablaProductos.addCell(celdaPrecioTotal);
+			tablaProductos.addCell(celdaCantidadProducto);
+			tablaProductos.addCell(celdaDescripcionProducto);
+			tablaProductos.addCell(celdaPrecioUnitario);
+			tablaProductos.addCell(celdaPrecioTotal);
 
-                for (int i = 0; i < tableVenta.getRowCount(); i++) {
-                    tablaProductos.addCell(tableVenta.getValueAt(i, 2).toString());
-                    tablaProductos.addCell(tableVenta.getValueAt(i, 1).toString());
-                    tablaProductos.addCell(tableVenta.getValueAt(i, 3).toString());
-                    tablaProductos.addCell(tableVenta.getValueAt(i, 4).toString());
-                }
+			for (int i = 0; i < tableVenta.getRowCount(); i++) {
+				tablaProductos.addCell(tableVenta.getValueAt(i, 2).toString());
+				tablaProductos.addCell(tableVenta.getValueAt(i, 1).toString());
+				tablaProductos.addCell(tableVenta.getValueAt(i, 3).toString());
+				tablaProductos.addCell(tableVenta.getValueAt(i, 4).toString());
+			}
 
-                documento.add(tablaProductos);
+			documento.add(tablaProductos);
 
-                Paragraph info = new Paragraph();
-                info.add(Chunk.NEWLINE);
-                info.add("Total a Pagar $" + totalPagar);
-                info.setAlignment(Element.ALIGN_RIGHT);
-                documento.add(info);
+			Paragraph info = new Paragraph();
+			info.add(Chunk.NEWLINE);
+			info.add("Total a Pagar $" + totalPagar);
+			info.setAlignment(Element.ALIGN_RIGHT);
+			documento.add(info);
 
-                Paragraph firma = new Paragraph();
-                firma.add(Chunk.NEWLINE);
-                firma.add("CancelaciÃ³n y Firma\n\n");
-                firma.add("_______________________");
-                firma.setAlignment(Element.ALIGN_CENTER);
-                documento.add(firma);
+			Paragraph firma = new Paragraph();
+			firma.add(Chunk.NEWLINE);
+			firma.add("Cancelación y Firma\n\n");
+			firma.add("_______________________");
+			firma.setAlignment(Element.ALIGN_CENTER);
+			documento.add(firma);
 
-                Paragraph mensaje = new Paragraph();
-                mensaje.add(Chunk.NEWLINE);
-                mensaje.add("Gracias por su compra");
-                mensaje.setAlignment(Element.ALIGN_CENTER);
-                documento.add(mensaje);
+			Paragraph mensaje = new Paragraph();
+			mensaje.add(Chunk.NEWLINE);
+			mensaje.add("Gracias por su compra");
+			mensaje.setAlignment(Element.ALIGN_CENTER);
+			documento.add(mensaje);
 
-                documento.close();
-                archivo.close();
-                Desktop.getDesktop().open(chosenFile);
-            }
-        } catch (DocumentException | IOException e) {
-            System.err.println("Error al generar reporte de la venta en PDf " + e.toString());
-        }
-    }
+			documento.close();
+			archivo.close();
+			Desktop.getDesktop().open(chosenFile);
+		}
+	}
 }
